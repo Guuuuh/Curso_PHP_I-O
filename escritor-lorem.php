14ac98e15b20bf6e81d62d529b6fad9320660db4
Guuuuh <gustavo.htl@hotmail.com>
2023-01-25 20:25:53 -0300

    usando o append para escrever no arquivo

--- escritor-lorem.php
+++ escritor-lorem.php
@@ -1,9 +1,9 @@
 <?php
 
-//cria o arquivo caso não exista para abrir ele, w = write
-$arquivo = fopen('lorem-escrito.txt', 'w');
+//cria o arquivo caso não exista para abrir ele, a = append
+$arquivo = fopen('lorem-escrito.txt', 'a');
 
-$lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+$lorem = "\nPellentesque habitant morbi tristique senectus et netus.";
 
 //em qual arquivo deseja escrever, o conteúdo, e o quanto deseja escrever.
 fwrite($arquivo, $lorem);
